0843e38d511493406ff77f032b292e48144ef56f
tahrer007 <tahrer_Ad@outlook.com>
2022-11-13 00:34:37 +0200

    correct wrong sentax

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { setState, useEffect } from "react";
+import { setState, useEffect, useState } from "react";
 import { QUESTIONS_NUMBER } from "./constants/constants";
 import Result from "./components/Result/Result";
 import GameBoard from "./components/gameBoard/GameBoard";
 import "./App.css";
 
 function App() {
-  const [answered, setAnswered] = setState(0);
-  const [correctAnswers, setCorrectAnswers] = setState(0);
+  const [answered, setAnswered] = useState(0);
+  const [correctAnswers, setCorrectAnswers] = useState(0);
 
   const add = (correct) => {
     if (correct) setCorrectAnswers(correctAnswers + 1);
