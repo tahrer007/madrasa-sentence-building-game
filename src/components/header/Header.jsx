cd0e2f747edf64b3cd33891bbcb654806d354e93
tahrer007 <tahrer_Ad@outlook.com>
2022-11-23 23:51:59 +0200

    stop watch

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect ,useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faVolumeHigh, faVolumeMute } from "@fortawesome/free-solid-svg-icons";
 import { QUESTIONS_NUMBER } from "constants/constants";
@@ -6,26 +6,38 @@ import ProgressBar from "@ramonak/react-progress-bar";
 
 import "./header.scss";
 
-function Header({ playSound, progress ,gameEnd }) {
+function Header({ playSound, progress, gameEnd }) {
   const [timer, setTimer] = useState(null);
-  const [isActive, setIsActive] = useState(false) ;
+  const [isActive, setIsActive] = useState(false);
   const [mute, setMute] = useState(false);
-  const countRef = useRef(null) ;
+  const increment = useRef(null);
+  useEffect(() => {
+    increment.current = setInterval(() => {
+      setTimer((timer) => timer + 1);
+    }, 1000);
+  }, []);
 
   useEffect(() => {
-    
-  }, []);
+    console.log(gameEnd);
+    if (gameEnd) {
+      clearInterval(increment.current);
+    }
+  }, [gameEnd]);
 
   useEffect(() => {
     playSound(mute);
   }, [mute, playSound]);
 
- 
-
+  const formatTime = () => {
+    const getSeconds = `0${timer % 60}`.slice(-2);
+    const minutes = `${Math.floor(timer / 60)}`;
+    const getMinutes = `0${minutes % 60}`.slice(-2);
+    return `${getMinutes}:${getSeconds}`;
+  };
 
   return (
     <header>
-      <div className="time-container">time</div>
+      <div className="time-container">{formatTime()}</div>
       <div className="progress-container">
         <ProgressBar
           completed={progress}
