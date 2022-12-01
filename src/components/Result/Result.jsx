22739a1d9a331b5f872479a0ddecd855bcdc520e
tahrer007 <tahrer_Ad@outlook.com>
2022-12-01 09:41:00 +0200

    result main image and component

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { useEffect } from "react";
+import { QUESTIONS_NUMBER ,RESULT_DATA} from "constants/constants";
 
-function Result({totalPoints}) {
+
+
+function Result({ totalPoints }) {
+  const win = totalPoints > QUESTIONS_NUMBER / 2;
+  const {text, imgSrc} = win ? RESULT_DATA.win : RESULT_DATA.lose ;
+
   return (
-    <>Result : {totalPoints}</>
-  )
+    <>
+      <p>
+        ניקוד: <span className="score">{totalPoints} / {QUESTIONS_NUMBER}</span>
+      </p>
+      <span className="result-msg"> {text} </span>
+      <div className="result-image">
+        <img
+          className="lose"
+          src={imgSrc}
+          width="120"
+          alt=""
+        />
+      </div>
+    </>
+  );
 }
 
-export default Result
+export default Result;
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,17 @@
-const QUESTIONS_NUMBER = 6 ; 
+const QUESTIONS_NUMBER = 6;
 
+const RESULT_DATA = {
+  win: {
+    imgSrc:
+      "https://upload.wikimedia.org/wikipedia/commons/3/33/Twemoji12_1f627.svg",
+    text: "כל אלאחתראם! כל הכבוד!",
+  },
 
-export {QUESTIONS_NUMBER} 
+  lose: {
+    imgSrc:
+      "https://upload.wikimedia.org/wikipedia/commons/3/33/Twemoji12_1f627.svg",
+    text: "בַּסִיטַה! לא הצלחתם הפעם. נסו שוב!",
+  },
+};
+
+export { QUESTIONS_NUMBER, RESULT_DATA };
